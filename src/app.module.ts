81e9fdd90bf4ac64fc9871c1a186910072026274
umlink <link_0403@163.com>
2024-06-11 16:09:31 +0800

    feat: init

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,31 @@ import { APP_GUARD, APP_FILTER } from '@nestjs/core';
 import { JwtAuthGuard } from '@/utils/guard/auth.guard';
 import { HttpExceptionFilter } from './utils/filters/http-exception-filters';
 import { RolesGuard } from '@/utils/guard/roles.guard';
-
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+/**
+ * ThrottlerModule: 限流
+ * 一秒钟内调用次数不超过 3 次、10 秒内调用次数不超过 20 次、一分钟内调用次数不超过 100 次
+ */
 @Module({
   imports: [
     ConfigModule.register({ folder: 'config' }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     TestModule,
     AuthModule,
     CatsModule,
@@ -22,18 +43,23 @@ import { RolesGuard } from '@/utils/guard/roles.guard';
   providers: [
     {
       provide: APP_GUARD,
-      useClass: JwtAuthGuard,
+      useClass: JwtAuthGuard, // 授权认证
     },
     {
       provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
+      useClass: HttpExceptionFilter, // 异常捕获
     },
     {
       provide: APP_GUARD,
-      useClass: RolesGuard,
+      useClass: RolesGuard, // 角色守卫
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard, // 全局限流
     },
   ],
 })
+// 设置中间件，可指定路由可方法
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,30 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import fastifyCsrf from '@fastify/csrf-protection';
+import helmet from '@fastify/helmet';
 import { ResultFormatInterceptor } from './utils/interceptor/result-format-interceptor';
 
+/**
+ * ⚠️底层使用 fastify
+ */
 async function mainApp() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ logger: true }),
+    new FastifyAdapter({ logger: true }), // 开启默认日志
   );
+  // 保护应用免受一些众所周知的 Web 漏洞的攻击
+  await app.register(helmet);
+  // CSRF 保护
+  await app.register(fastifyCsrf);
+  // 启用跨源资源共享（CORS）保护机制
+  app.enableCors();
+  // 设置接口统一前缀
+  // app.setGlobalPrefix('api');
+  // 格式化响应体
+  app.useGlobalInterceptors(new ResultFormatInterceptor());
+  // 注册全局函数式中间件
+  app.use(logger);
   /**
    * 开启方式后期需定制处理，如：线上自动关闭
    */
@@ -24,16 +41,7 @@ async function mainApp() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger-api', app, document);
-  /**
-   * 设置接口统一前缀
-   */
-  // app.setGlobalPrefix('api');
-  app.useGlobalInterceptors(new ResultFormatInterceptor());
-  /**
-   * 注册全局函数式中间件
-   */
-  app.use(logger);
-  //********************************************
+  /************************************************************************/
   await app.listen(8088, '0.0.0.0');
 }
 mainApp().then(() => console.info('server started by 0.0.0.0:8088'));

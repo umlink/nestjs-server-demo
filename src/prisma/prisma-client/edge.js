fe06198a302cc41db3793015d4f9b1839064d9ff
umlink <link_0403@163.com>
2024-06-14 15:16:25 +0800

    feat: init

--- src/prisma/prisma-client/edge.js
+++ src/prisma/prisma-client/edge.js
@@ -134,8 +134,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "..",
   "clientVersion": "5.15.0",
--- src/prisma/prisma-client/index.js
+++ src/prisma/prisma-client/index.js
@@ -135,8 +135,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "..",
   "clientVersion": "5.15.0",

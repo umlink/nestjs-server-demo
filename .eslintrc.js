642304a283232dd07a1e70049ba01c331e3d773a
umlink <link_0403@163.com>
2024-06-19 15:46:31 +0800

    feat: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'src/prisma'],
+  ignorePatterns: ['.eslintrc.js', 'prisma'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',

305d2881974ecb906df1b1bb770314e1092f4341
umlink <link_0403@163.com>
2024-06-26 16:12:41 +0800

    feat: init

--- src/modules/template/vo/template.vo.ts
+++ src/modules/template/vo/template.vo.ts
@@ -1,16 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class TemplateItemVO {
+  @ApiProperty()
   id: string;
+  @ApiProperty()
   code: string;
+  @ApiProperty()
   title: string;
+  @ApiProperty()
   content: JSON;
+  @ApiProperty()
   headerImg: string;
+  @ApiProperty()
   isVip: number;
+  @ApiProperty()
   useCount: number;
 }
 
 export class TemplateListVo {
+  @ApiProperty()
   list: TemplateItemVO[];
+  @ApiProperty()
   total: number;
+  @ApiProperty()
   pageNum: number;
+  @ApiProperty()
   pageSize: number;
 }

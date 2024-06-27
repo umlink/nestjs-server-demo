73a16985d735200d9ecf62390bf71fda7fba5fb7
umlink <link_0403@163.com>
2024-06-27 15:07:37 +0800

    feat: init

--- src/modules/upload/dto/create-upload.dto.ts
+++ src/modules/upload/dto/create-upload.dto.ts
@@ -1,7 +1,6 @@
-import { Multipart } from '@fastify/multipart';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUploadDto {
   @ApiProperty({ type: 'multipart/form-data', description: '上传文件 multipart/form-data' })
-  file: Multipart;
+  file: string;
 }
